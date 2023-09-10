87c80c5f39712ae2d7595eee1a1fce59f4abd5c5
haiyen040602 <20021480@vnu.edu.vn>
2023-09-11 02:09:10 +0700

    edit

--- code/gene_data_parallel.py
+++ code/gene_data_parallel.py
@@ -10,7 +10,7 @@ model_name = "gpt2-medium"
 model = GPT2LMHeadModel.from_pretrained(model_name, output_hidden_states=True)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-def run_inference(rank, world_size):
+def run_inference(rank, knobs, prompts, topics, affects, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '5554'
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
@@ -19,29 +19,29 @@ def run_inference(rank, world_size):
     model.eval()
 
     if dist.get_rank() == 0:
-        prompt = "also I"
-        affect = None
-        topic = 'positive'
-        knob = 0.2
+        prompt = prompts[0]
+        affect = affects[0]
+        topic = topics[0]
+        knob = knobs[0]
     elif dist.get_rank() == 1:
-        prompt = "there are"
-        affect = "anger"
-        topic = 'negative'
-        knob = 0.4
+        prompt = prompts[1]
+        affect = affects[1]
+        topic = topics[1]
+        knob = knobs[1]
     
     generated_text = emotional_gene(Knob=knob, Prompt=prompt, Topic=topic, Affect=affect, model = model, tokenizer=tokenizer, device = rank)
     print(generated_text)
 
-def main():
+def run_emo_gene(knobs=[0.4, 0.5], prompts=['There are', 'also I'], topics=[None, 'positive'], affects=[None, 'joy']):
     world_size = 2
     mp.spawn(
         run_inference,
-        args=(world_size,),
+        args=(world_size,knobs, prompts, topics, affects, ),
         nprocs=world_size,
         join=True
     )
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
     
